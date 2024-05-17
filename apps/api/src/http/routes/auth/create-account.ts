5ff5c32e79cbb2bf43a81adad08ea364be155b95
Farias-sys <75812534+Farias-sys@users.noreply.github.com>
2024-05-17 00:22:34 -0300

    chore: Create user route

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
+import {prisma} from '../../../lib/prisma'
+import {hash} from 'bcryptjs'
+
 
 export async function createAccount(app : FastifyInstance){
     app.withTypeProvider<ZodTypeProvider>().post('/users',{
@@ -11,7 +14,29 @@ export async function createAccount(app : FastifyInstance){
                 password: z.string().min(6)
             })
         }
-    } , () => {
-        return 'User created!'
+    } , async (request, reply) => {
+        const {name, email, password} = request.body;
+
+        const userWithSameEmail = await prisma.user.findUnique({
+            where: {email}
+        });
+
+        if(userWithSameEmail){
+            return reply
+            .status(400)
+            .send({message:'user with same e-mail already exists.'})
+        };
+
+        const passwordHash = await hash(password, 6);
+
+        await prisma.user.create({
+            data: {
+                name, 
+                email,
+                passwordHash
+            }
+        })
+
+        return reply.status(201).send()
     })
 }
